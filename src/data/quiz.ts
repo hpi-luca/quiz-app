cff24533c15a10b3f5febde71c69f53a004ed89c
Arthur BRUNOT <arthur.brunot.pro@gmail.com>
2021-01-15 19:50:55 +0100

    last changes

--- src/data/quiz.ts
+++ src/data/quiz.ts
@@ -1,4 +1,7 @@
 import { Question, ResultRange } from "../store"
+import red from "../assets/images/red.svg"
+import orange from "../assets/images/orange.svg"
+import green from "../assets/images/green.svg"
 
 const quizQuestions: Question[] = [
   {
@@ -76,19 +79,19 @@ export const plagesResultat: ResultRange[] = [
     scoreMin: 4,
     level: "vert",
     message: "<h3 style='font-size: 25px; line-height:2;'>Vos stéréotypes de genre sont minimes.</h3><p>Pour vous l’égalité entre les hommes et les femmes au travail est une évidence. Les compétences ne dépendent pas du genre d’une personne et le sexisme au travail vous dérange, voire vous parait inacceptable. <br><br>D’ailleurs, depuis une loi du 17 août 2015, les agissements sexistes, plus connus sous le nom de sexisme ordinaire, sont pénalement condamnables. <br><br>N’hésitez pas à en parler à vos collègues et à intervenir lorsque vous êtes témoins d’agissements sexistes au travail.<br><br><b>Rendez-vous la semaine prochaine sur l’intranet pour poursuivre la sensibilisation sur le sexisme!</b></p>",
-    illustration: "src/assets/images/green.svg",
+    illustration: green,
   },
   {
     scoreMin: 7,
     level: "orange",
     message: "<h3 style='font-size: 25px; line-height:2;'>Vos stéréotypes de genre sont modérés. </h3><p>Des stéréotypes nous en avons tous. Ils se construisent depuis l’enfance, permettent à notre cerveau de décrypter le monde rapidement et sont plus ou moins accentués selon nos croyances et nos schémas normatifs de pensée. <br><br>Vous avez plutôt conscience de ces stéréotypes et c’est important pour ne pas les laisser prendre le pouvoir de notre perception et de nos actes au risque d’inférioriser un sexe par rapport à un autre. <br><br>D’ailleurs, depuis une loi du 17 août 2015, les agissements sexistes, plus connus sous le nom de sexisme ordinaire, sont pénalement condamnables. <br><br><b>Rendez-vous la semaine prochaine sur l’intranet pour poursuivre la sensibilisation sur le sexisme!</b></p>",
-    illustration: "src/assets/images/orange.svg",
+    illustration: orange,
   },
   {
     scoreMin: 10,
     level: "rouge",
     message: "<h3 style='font-size: 25px; line-height:2;'>Vos stéréotypes de genre sont prononcés.</h3><p>Des stéréotypes nous en avons tous. Ils se construisent depuis l’enfance, permettent à notre cerveau de décrypter le monde rapidement et sont plus ou moins accentués selon nos croyances et nos schémas normatifs de pensée.<br><br> Il est très important d’en avoir conscience pour ne pas laisser nos stéréotypes prendre le pouvoir de notre perception et de nos actes au risque d’inférioriser un sexe par rapport à un autre. <br><br>D’ailleurs, depuis une loi du 17 août 2015, les agissements sexistes, plus connus sous le nom de sexisme ordinaire, sont pénalement condamnables. <br><br><b>Rendez-vous la semaine prochaine sur l’intranet pour poursuivre la sensibilisation sur le sexisme!</b></p>",
-    illustration: "src/assets/images/red.svg",
+    illustration: red,
   },
 ]
 
